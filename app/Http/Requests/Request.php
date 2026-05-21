d1465d8a360eddd23330678c7042dc772c6e1159
rafaadail <rafaadailph@gmail.com>
2026-05-20 23:24:22 -0300

    feat(testing): add authentication feature tests

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Arr;
+
+class Request extends FormRequest
+{
+    /**
+    * Get the error messages for the defined validation rules.*
+    * @return array
+    */
+    protected function failedValidation(Validator $validator)
+    {
+        $message = "Erro de validação.";
+        
+        $message = implode(PHP_EOL, $validator->errors()->all());
+
+        if(isset($validator->failed()['USUARIO']['App\Rules\ValidaLoginRule'])){
+            $message = $validator->errors()->first('USUARIO');
+        }
+
+        throw new HttpResponseException(response()->json([
+        "error" => 1,
+        "message" => $message,
+        'errors' => $validator->errors()->all(),
+        ], 422));
+    }
+}
